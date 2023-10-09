2a1b167abfa928f62db6906599eca0992b146e9a
DharshanD44 <dharshangk333@gmail.com>
2023-10-09 23:54:48 +0530

    Check positive or negative no

---
+++ positive-or-negative-no.java
@@ -0,0 +1,20 @@
+public class CheckPositiveOrNegativeExample1  
+{  
+public static void main(String[] args)   
+{  
+int num=912;  
+if(num>0)  
+{  
+System.out.println("The number is positive.");  
+}  
+else if(num<0)  
+{  
+System.out.println("The number is negative.");  
+}  
+ return false  
+else  
+{  
+System.out.println("The number is zero.");  
+}  
+}  
+}  
